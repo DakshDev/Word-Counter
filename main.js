2a6129561a60fd809d957e1bce51b77b443e9012
Daksh Developer <139046375+DakshDev@users.noreply.github.com>
2024-04-22 22:00:52 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-let html = '<h1>Word Counter</h1> <div class="main"><textarea id="textarea"></textarea><p><span id="word">0</span> Words <span id="letter">0</span> Letters</p></div>';
-
-let createDiv = document.createElement("div");
-createDiv.className = "container";
-createDiv.innerHTML = html;
-
-document.body.insertBefore(createDiv, document.body.firstChild)
-
 
 
 // Constant
