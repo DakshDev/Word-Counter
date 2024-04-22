cb05ae5394000cb30ae3e403d1aa015712fc8188
Daksh Developer <139046375+DakshDev@users.noreply.github.com>
2024-04-22 21:42:40 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+let html = '<div class="container">
+    <h1>Word Counter</h1>
+    <div class="main"><textarea id="textarea"></textarea><p><span id="word">0</span> Words <span id="letter">0</span> Letters</p></div>
+</div>';
+
+let createDiv = document.createElement("div");
+createDiv.innerHTML = html;
+
+document.body.insertBefore(createDiv, document.body.firstChild)
+
+
+
 // Constant
 const textarea = document.getElementById('textarea');
 
