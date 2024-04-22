901be2323d832743ad7c7a11ad98d96a198f42fc
Daksh Developer <139046375+DakshDev@users.noreply.github.com>
2024-04-22 22:09:25 +0500

    Upload

---
+++ script.js
@@ -0,0 +1,25 @@
+    
+let html = `<h1>Word Counter</h1>
+<div class="main">
+    <textarea id="textarea"></textarea>
+    <p><span id="word">0</span> Words <span id="letter">0</span> Letters</p>
+</div>`;
+
+let createDiv = document.createElement("div");
+createDiv.className = "container";
+createDiv.innerHTML = html;
+
+document.body.insertAdjacentElement("afterbegin",createDiv);
+
+const textarea = document.getElementById('textarea');
+
+textarea.addEventListener('input',function(){
+    document.getElementById('letter').innerHTML = this.value.length;
+
+    let split = this.value.split(' ');
+    let cleanList = split.filter(function(elm){
+        return elm !== '';
+    });
+    document.getElementById('word').innerHTML = cleanList.length;
+});      
+
